58694b0f614164b07af967a960ee5fba7111f65a
Sam-Teche <ogunrindesam@gmail.com>
2025-07-20 20:30:07 +0100

    Your commit message

--- routes/auth.js
+++ routes/auth.js
@@ -87,7 +87,7 @@ router.post("/login", loginValidation, async (req, res) => {
       sameSite: "lax",
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
       path: "/",
-     
+      domain: ".trentbank.netlify.app",
     });
 
 
@@ -219,7 +219,7 @@ router.post("/logout", (req, res) => {
   res.clearCookie("refreshToken", {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
+    sameSite: "lax",
     path: "/",
   });
 
